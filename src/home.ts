3a67878459148bd6837d4ee40418713800262fd7
ecrseer <juustino@live.com>
2021-10-03 14:52:40 -0300

    wow

--- src/home.ts
+++ src/home.ts
@@ -1,12 +1,16 @@
+require('dotenv').config()
+
 export default function searchImagemByName(req:any, res:any) {
   let nome = req.query.palavraChave || 'Abelha';
 
   const GoogleImages = require('google-images');
     console.log("id eh "+process.env.CSE_ID)
+
   const client = new GoogleImages(
     process.env.CSE_ID,
     process.env.GPROJECT_API_KEY,
   );
+
 
 
   client.search(nome).then((images: any) => {
